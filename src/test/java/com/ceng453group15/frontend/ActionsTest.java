7b1420af9d6b909d46aefa6f5966c0c91a7e6d01
sariburak <burock.2046@gmail.com>
2022-06-15 10:42:32 +0300

    Adding testcase for PayRentAction

--- src/test/java/com/ceng453group15/frontend/ActionsTest.java
+++ src/test/java/com/ceng453group15/frontend/ActionsTest.java
@@ -2,12 +2,15 @@ package com.ceng453group15.frontend;
 
 import com.ceng453group15.frontend.GameLogic.Player;
 import com.ceng453group15.frontend.GameLogic.PlayerStates.JailState;
+import com.ceng453group15.frontend.GameLogic.PlayerStates.WaitState;
 import com.ceng453group15.frontend.GameLogic.Property;
+import com.ceng453group15.frontend.GameLogic.PropertyType;
 import com.ceng453group15.frontend.GameLogic.TileActions.GoToJailAction;
 import com.ceng453group15.frontend.GameLogic.TileActions.PayRentAction;
 import com.ceng453group15.frontend.GameLogic.TileActions.PayTaxAction;
 import org.junit.jupiter.api.Test;
 
+import static com.ceng453group15.frontend.GameLogic.GameConstants.DEFAULT_PLAYER_BUDGET;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ActionsTest {
@@ -29,10 +32,21 @@ public class ActionsTest {
         payTaxAction.execute(player);
 
         assertTrue(player.getBudget() == 1450);
+        assertTrue(player.getPlayerState() instanceof WaitState);
     }
 
     @Test
     void PayRentAction_success(){
+        int price = 100;
+        Property property = new Property(price, PropertyType.REGULAR);
+        Player owner = new Player();
+        Player payer = new Player();
+        PayRentAction action = new PayRentAction(property, owner);
 
+        action.execute(payer);
+
+        assertTrue(owner.getBudget() == DEFAULT_PLAYER_BUDGET + price * 0.1);
+        assertTrue(payer.getBudget() == DEFAULT_PLAYER_BUDGET - price * 0.1);
+        assertTrue(payer.getPlayerState() instanceof WaitState);
     }
 }
