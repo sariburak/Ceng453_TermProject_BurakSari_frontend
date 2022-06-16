5ddb420cbdce16030fe8d306ca9edc688ac7f31f
sariburak <burock.2046@gmail.com>
2022-06-17 02:26:09 +0300

    Adding city name placeholders

--- src/main/java/com/ceng453group15/frontend/Controllers/GameController.java
+++ src/main/java/com/ceng453group15/frontend/Controllers/GameController.java
@@ -4,14 +4,34 @@ import com.ceng453group15.frontend.CustomPane;
 import javafx.animation.*;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
+import javafx.scene.text.Text;
 import javafx.util.Duration;
 
 public class GameController {
 
+    @FXML
+    public GridPane mainPane;
+
+    @FXML
+    public Pane pane04;
+
+    public void mainPaneClicked(MouseEvent mouseEvent) {
+        System.out.println("Mouse event on main pane is triggered");
+
+        for(Node node: pane04.getChildren()){
+            Text text = (Text) node;
+            System.out.println(text.getText());
+            text.setText("Changed");
+            text.setLayoutX(pane04.getLayoutX() + (pane04.getWidth() - text.getLayoutBounds().getWidth())/2);
+        }
+    }
 }
